e6b931a874ec06a6817edf5dc8be40714ca367e2
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-03-19 20:17:39 +0200

    files updated

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,5 +1,6 @@
 import { React, Component } from 'react';
-import Controls from './Controls';
+
+import Value from './Value';
 import './Counter.css';
 
 class Counter extends Component {
@@ -31,17 +32,18 @@ class Counter extends Component {
     return (
       <div className="Counter">
         <span className="Counter__value">{this.state.value}</span>
+        {/* <Value value={this.state.value} /> */}
 
-        <Controls onIncrement={this.handleIncrement} onDecrement={this.handleDecrement} />
+        {/* <Controls onIncrement={this.handleIncrement} onDecrement={this.handleDecrement} /> */}
 
-        {/* <div className="Counter__controls">
+        <div className="Counter__controls">
           <button type="button" onClick={this.handleIncrement}>
             Збільшити на 1
           </button>
           <button type="button" onClick={this.handleDecrement}>
             Зменшити на 1
           </button>
-        </div> */}
+        </div>
       </div>
     );
   }
