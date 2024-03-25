40668c8a2991c2681f2c371f5eaa9d28ab79f20e
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-03-25 20:47:27 +0200

    Form updated

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
 
 class Form extends Component {
   state = {
     name: '',
     tag: '',
   };
+
+  nameInputId = nanoid();
+  tagInputId = nanoid();
 
   handleChange = e => {
     const { name, value } = e.currentTarget;
@@ -25,14 +29,26 @@ class Form extends Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
+        <label htmlFor={this.nameInputId}>
           Ім'я
-          <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+            id={this.nameInputId}
+          />
         </label>
 
-        <label>
+        <label htmlFor={this.tagInputId}>
           Нікнейм
-          <input type="text" name="tag" value={this.state.tag} onChange={this.handleChange} />
+          <input
+            type="text"
+            name="tag"
+            value={this.state.tag}
+            onChange={this.handleChange}
+            id={this.tagInputId}
+          />
         </label>
 
         <button type="submit">Відправити</button>
