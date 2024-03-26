37f12b71b59846a51df989d6b0fa7ab23a43503f
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-03-26 14:26:33 +0200

    Form updated

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,6 +5,8 @@ class Form extends Component {
   state = {
     name: '',
     tag: '',
+    experience: 'junior',
+    licence: 'false',
   };
 
   nameInputId = nanoid();
@@ -20,6 +22,12 @@ class Form extends Component {
     event.preventDefault();
     this.props.onSubmit(this.state);
     this.reset();
+  };
+
+  handleLicenceChange = event => {
+    console.log(event.currentTarget.checked);
+
+    this.setState({ licence: event.currentTarget.checked });
   };
 
   reset = () => {
@@ -51,7 +59,53 @@ class Form extends Component {
           />
         </label>
 
-        <button type="submit">Відправити</button>
+        <p>Ваш рівень:</p>
+        <label>
+          <input
+            type="radio"
+            name="experience"
+            value="junior"
+            onChange={this.handleChange}
+            checked={this.state.experience === 'junior'}
+          />
+          Junior
+        </label>
+
+        <label>
+          <input
+            type="radio"
+            name="experience"
+            value="middle"
+            onChange={this.handleChange}
+            checked={this.state.experience === 'middle'}
+          />
+          Middle
+        </label>
+
+        <label>
+          <input
+            type="radio"
+            name="experience"
+            value="senior"
+            onChange={this.handleChange}
+            checked={this.state.experience === 'senior'}
+          />
+          Senior
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            name="licence"
+            checked={this.state.licence}
+            onChange={this.handleLicenceChange}
+          />
+          Згоден з умовами офера
+        </label>
+
+        <button type="submit" disabled={!this.state.licence}>
+          Відправити
+        </button>
       </form>
     );
   }
